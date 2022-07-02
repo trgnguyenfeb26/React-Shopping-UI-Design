9dcd45129b14a7c7b9b4ef3ccf26227ad4769b4a
Trung Nguyên <nguyennguyentrung01@gmail.com>
2022-07-03 01:23:27 +0700

    slide

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const slideItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/hWydrZm/slide.jpg",
+        tilet:"EVERYDAY WHITE",
+        desc: "Spruce Up Your Everyday Basic Look",
+        bg:"f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/GtZwmD2/slide2.jpg",
+        tilet:"HELIANTHUS",
+        desc: "Immerse Yourself In The Midst Of Nature",
+        bg:"fcf1ed",
+    },
+
+]
